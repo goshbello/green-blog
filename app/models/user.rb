e5c3e7a756809b61b361b863efb136448d6f3af2
Mc <goshbello@hotmail.com>
2022-02-24 18:03:59 +0000

    add user table and validation

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+
+  validates :username, presence: true, 
+             uniqueness: { case_sensitive: false },  
+             length: { minimum: 3, maximum: 25 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, 
+             uniqueness: { case_sensitive: false },
+             length: { maximum: 105 },
+             format: {with: VALID_EMAIL_REGEX}
+
+
+end
+
+
+# uniqueness: true ==> This will not check for case sensitivity. Meaning Aaa != aaa. Meaning username can be Aaa or aaa.
+
+# uniqueness: { case_sensitive: false }  ==> Meaning Aaa == aaa. Meaning if one username is Aaa another username can't be aaa.
