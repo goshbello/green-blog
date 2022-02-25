8355db5477d2824770415d0932f1f898353c1ef1
goshbello <44555654+goshbello@users.noreply.github.com>
2022-02-25 23:53:40 +0000

    add user signup and updated UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Green Blog #{@user.username}, You have successfully signed up"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
   resources :articles
+
+  # routes to authenticate user - signup
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create' # post request to send completed form to the user create action
+  resources :users, except: [:new] # we replace the line 10 with this. we have already define new action in line 9, dont need new route again
 end
