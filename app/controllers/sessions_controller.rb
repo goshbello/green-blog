d7e8d35af4161abe8da64dc9ae64c750a648b7d6
goshbello <44555654+goshbello@users.noreply.github.com>
2022-02-28 16:24:34 +0000

    minus correction in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notices] = "Logged in successfully"
+      flash[:notice] = "Logged in successfully"
       redirect_to user
     else
       flash.now[:alert] = "There was something wrong with your login details"
